497211a551cc5d5a14335991070f548d5bb8461f
dnavarroefl <dnavarroefl@hotmail.com>
2015-06-03 18:06:04 +0200

    last changes

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,2 +1,11 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+button = document.getElementById('like-button')
+button.addEventListener('click', likePost, false)
+
+function likePost() {
+  button.classList.add('liked');
+  button.innerHTML = "Liked!";
+  document.querySelector('h3').style.color = "red";
+}
