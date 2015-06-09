b8dadd166b244a57bc99670e1f58f73d753213fd
dnavarroefl <dnavarroefl@hotmail.com>
2015-06-09 13:08:34 +0200

    added color to the game, external style link, round border for tables

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -1,11 +1,95 @@
-$('td#doRight').on('click', function() {
-	alert('Correct!');
-});
 
-$('td#doWrong').on('click', function() {
-	alert('Wrong!');
-});
+	$('td#doRight1').on('click', function() {
+		alert('Correct!');
+	});
 
+	$('td#doRight1').on('click', function() {
+		alert('Correct!');
+	});
+
+	$('td#doRight2').on('click', function() {
+		alert('Correct!');
+	});
+
+	$('td#doRight3').on('click', function() {
+		alert('Correct!');
+	});
+
+	$('td#doRight4').on('click', function() {
+		alert('Correct!');
+	});
+
+	$('td#doRight5').on('click', function() {
+		alert('Correct!');
+	});
+
+	$('td#doRight6').on('click', function() {
+		alert('Correct!');
+	});
+
+	$('td#doRight7').on('click', function() {
+		alert('Correct!');
+	});
+
+	$('td#doRight8').on('click', function() {
+		alert('Correct!');
+	});
+
+	$('td#doRight9').on('click', function() {
+		alert('Correct!');
+	});
+
+	$('td#doWrong1').on('click', function() {
+		alert('Wrong!');	
+	});
+
+	$('td#doWrong2').on('click', function() {
+		alert('Wrong!');	
+	});
+
+	$('td#doRight1').on('click', function() {
+		$('td#doRight1').css('background-color', 'rgb(73,203,214)');
+	});
+
+	$('td#doRight2').on('click', function() {
+		$('td#doRight2').css('background-color', 'rgb(73,203,214)');
+	});
+
+	$('td#doRight3').on('click', function() {
+		$('td#doRight3').css('background-color', 'rgb(73,203,214)');
+	});
+
+	$('td#doRight4').on('click', function() {
+		$('td#doRight4').css('background-color', 'rgb(73,203,214)');
+	});
+
+	$('td#doRight5').on('click', function() {
+		$('td#doRight5').css('background-color', 'rgb(73,203,214)');
+	});
+
+	$('td#doRight6').on('click', function() {
+		$('td#doRight6').css('background-color', 'rgb(73,203,214)');
+	});
+
+	$('td#doRight7').on('click', function() {
+		$('td#doRight7').css('background-color', 'rgb(73,203,214)');
+	});
+
+	$('td#doRight8').on('click', function() {
+		$('td#doRight8').css('background-color', 'rgb(73,203,214)');
+	});
+
+	$('td#doRight9').on('click', function() {
+		$('td#doRight9').css('background-color', 'rgb(73,203,214)');
+	});
+
+	$('td#doWrong1').on('click', function() {
+		$('td#doWrong1').css('background-color', 'red');
+	});
+
+	$('td#doWrong2').on('click', function() {
+		$('td#doWrong2').css('background-color', 'red');
+	});
 
 /*
 	var user = {}
