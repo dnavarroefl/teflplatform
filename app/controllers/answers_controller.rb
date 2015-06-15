6582b7cf7b56bd42139a84158fdd819956750143
dnavarroefl <dnavarroefl@hotmail.com>
2015-06-15 17:57:05 +0200

    added answers to seed, config routes, new look for the post form

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -7,6 +7,36 @@ class AnswersController < ApplicationController
 
 	def index
 		@answers = Answer.all
+		@tutors = Tutor.all 
 		@students = Student.all
 	end 
+
+	def show
+		@answer = Answer.find(params[:id])
+	end 
+
+	def new
+		@answer = Answer.new
+	end 
+
+	def edit
+		@answer = Answer.find(params[:id])
+	end 
+
+	def create
+		@answer = Answer.new(answer_params)
+     	@answer.save
+     	redirect_to answer_path(@answer)
+	end 
+
+	def update
+		@answer = Answer.find(params[:id])
+	  	@answer.update(answer_params)
+	  	redirect_to answer_path(@answer)
+	end 
+
+	def answer_params
+		params.require(:answer).permit(:title, :name, :text)
+	end 
 end
+
