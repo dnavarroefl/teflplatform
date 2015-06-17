8ef5a3f3eb64e0888fffcc881ef4c1faa23ee4dd
dnavarroefl <dnavarroefl@hotmail.com>
2015-06-17 17:49:06 +0200

    validation for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,29 @@
 class User < ActiveRecord::Base
-	#authenticates_with_sorcery!
-	validates_confirmation_of :password, message: "Both fields must be identical", if: :password
+ 	authenticates_with_sorcery!	
+
+  validates :password, length: { minimum: 3 }
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
+
+  validates :email, uniqueness: true
+
+=begin  
+  def self.authenticate(email, password)
+    user = find_by_email(email)
+    if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+      user
+    else
+      nil
+    end
+  end
+  
+  def encrypt_password
+    if password.present?
+      self.password_salt = BCrypt::Engine.generate_salt
+      self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+    end
+  end
+=end
 end
+
+
