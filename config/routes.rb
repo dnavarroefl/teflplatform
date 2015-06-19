347f1a7aea86b4dde4774506687983b996c001fd
dnavarroefl <dnavarroefl@hotmail.com>
2015-06-19 11:19:00 +0200

    new routes for comments on posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,26 @@
 Rails.application.routes.draw do
-   
+  
+  # Rutas para la autenticación 
   get 'sessions/new' 
   
   get "login" => "sessions#new", :as => "login"
-  get "signup" => "users#new", :as => "signup"
   get "logout" => "sessions#destroy", :as => "logout"
+  get "signup" => "users#new", :as => "signup"
 
-  resources :tutors, :students, :posts, :comments, :answers 
   resources :users
   resources :sessions
   root :to => 'students#home'
+
+  # Rutas para los usuarios y su interacción (posts, respuestas y comentarios)
+
+  resources :tutors, :students, :answers 
+  
+  resources :posts do 
+    resources :comments
+  end 
+
+  # Rutas para las secciones a las que podrán acceder los estudiantes y los profesores
+
   get '/home' => 'students#home' 
   get '/contact' => 'students#contact'
   get '/media' => 'students#media'
@@ -19,6 +30,8 @@ Rails.application.routes.draw do
   get '/tutor' => 'tutors#index'
 
   get '/teachers' => 'teachers#index'
+
+  # Rutas para las herramientas conectadas a nuestra plataforma
 
   get  "dropbox/main"
   get  "dropbox/auth_start"
